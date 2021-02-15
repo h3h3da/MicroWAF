8879f7f9f87acaf9569432a03f58b4f3fecec709
Crescentm <guyues@outlook.com>
2021-02-15 11:17:23 +0800

    fix logfile addr is not absolute

--- log.py
+++ log.py
@@ -1,18 +1,19 @@
 import time
-import pathlib
 from loguru import logger
 from config import LOG_PATH
 from sql import insert_log
+from pathlib import Path
 
 
 def log_block(addr, result):
     Format = "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | {level} | <level>{message}</level>"
-    path = pathlib.Path(LOG_PATH)
+    current_path = Path.cwd()
+    path = Path(current_path, LOG_PATH)
     if not path.exists():
         path.mkdir(parents=True)
     filename = time.strftime("%Y-%m-%d", time.localtime()) + '.log'
-    logfile = pathlib.Path(path, filename)
-    insert_log(logfile)
+    logfile = Path(path, filename)
+    insert_log(str(logfile))
     i = logger.add(logfile,
                rotation="00:00",
                retention="30 days",
